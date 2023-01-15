8e210d2274e76413fde1af554b21461fdc33aa07
Michal Krupiarz <michal.krupiarz@softur.com>
2023-01-15 23:18:48 +0100

    styling done

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,28 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 const app = express();
 
+//path for expres
+const viewPath = path.join(__dirname, '../templates/views')
+const staticPath = path.join(__dirname, '../public');
+const partialPath = path.join(__dirname, '../templates/partials')
 
+// setup handlebars view engine to hbs and where to look for templates
 app.set('view engine', 'hbs'); //handlebars set up
-app.use(express.static(path.join(__dirname,'../public')));
+app.set('views', viewPath);
+hbs.registerPartials(partialPath);
+
+//setup static direcotry to serve
+app.use(express.static(staticPath));
 
 app.get('', (req,res)=>{
     res.render('index',{
-        title: 'wheater app changed by me',
-        createdBy: 'Michal'
+        title: 'Wheater app changed by me',
+        createdBy: 'Michal',
+        name: 'Michal'
     })
 })
 
@@ -24,8 +36,9 @@ app.get('/about', (req,res)=>{
 
 app.get('/help', (req,res)=>{
     res.render('help',{
-        msg: 'This is the helpe message',
-        msg2: 'do not resist.'
+        title: 'This is title of help page',
+        msg2: 'do not resist.',
+        name: 'Michal'
     })
 })
 
@@ -33,6 +46,26 @@ app.get('/forecast', (req,res)=>{
     res.send({
         location: 'Mietkow',
         temperature: '10 degrees'
+    })
+    res.render('forecast',{
+        title: 'this is forecast',
+        name: 'Michal'
+    })
+})
+
+app.get('/help/*', (req,res)=>{
+    res.render('pageNotFound',{
+        title: 'Sub page of help was not found.',
+        name: 'Michal',
+        errorMessage: 'Help does not have the article you are looking for'
+    })
+})
+
+app.get('*', (req,res)=>{
+    res.render('pageNotFound',{
+        title: 'Page not found',
+        name: 'Michal',
+        errorMessage: 'Page on server was not found'
     })
 })
 
