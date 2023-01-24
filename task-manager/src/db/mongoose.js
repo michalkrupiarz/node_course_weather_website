7ffb7a57c664338401490200291e48b359cf1713
Michal Krupiarz <michal.krupiarz@softur.com>
2023-01-24 18:24:25 +0100

    mongoose added

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
+
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trime: true
     },completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
-const laundry = new Task({description: "this is laundry", completed: false})
+const laundry = new Task({completed: true})
 
 laundry.save().then(() => {
     console.log(laundry)
@@ -19,14 +24,44 @@ laundry.save().then(() => {
 })
 // const User = mongoose.model('User', {
 //     name: {
-//         type: String
+//         type: String,
+//         required: true,
+//         trim: true
 //     },
 //     age: {
-//         type: Number
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if(value<0){
+//                 throw new Error('Age must be a positive number.')
+//             }
+//         }
+//     },
+//     email:{
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if(!validator.isEmail(value)){
+//                 throw new Error('Email is not valid')
+//             }
+//         }
+//     }, 
+//     password:{
+//         type: String,
+//         required: true,
+//         minLength: 7,
+//         trim: true,
+//         validate(value){
+//             if(validator.contains('password')){
+//                 throw new Error ('Password contains word password')
+//             }
+//         }
 //     }
 // })
 
-// const me = new User({name: "Michal", age: 38})
+// const me = new User({name: "   Zdzicho ", email: 'mike@GMAIL.com', password: 'password'})
 
 // me.save().then(()=>{
 //     console.log(me)
